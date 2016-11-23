e5c77d723abae207529accb6504bf56409f12135
Eric Lai <eldeveloper13@gmail.com>
2016-11-23 00:19:35 -0500

    Add Google Drive API query (Not working)

--- app/src/main/java/com/eldeveloper13/googleplaytestproject/MainActivity.java
+++ app/src/main/java/com/eldeveloper13/googleplaytestproject/MainActivity.java
@@ -1,16 +1,24 @@
 package com.eldeveloper13.googleplaytestproject;
 
 import android.app.Dialog;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataBuffer;
+import com.google.android.gms.drive.query.Query;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,13 +35,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         checkGMSVersion();
+        setupGoogleApiClient();
+    }
 
+    @OnClick(R.id.async_load_file)
+    void onAsyncLoadFileClicked() {
+        loadFile("");
+    }
+
+    private void loadFile(String filename) {
+        Query query = new Query.Builder()
+                .build();
+        Drive.DriveApi.query(mGoogleApiClient, query)
+                .setResultCallback(new ResultCallback<DriveApi.MetadataBufferResult>() {
+                    @Override
+                    public void onResult(@NonNull DriveApi.MetadataBufferResult metadataBufferResult) {
+                        if (metadataBufferResult.getMetadataBuffer() != null) {
+                            MetadataBuffer metadataBuffer = metadataBufferResult.getMetadataBuffer();
+                            try{
+                                for (Metadata metadata : metadataBuffer) {
+                                    Log.d(MainActivity.this.getLocalClassName(), metadata.toString());
+                                }
+                            } finally {
+                                metadataBuffer.release();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error getting file ", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void setupGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this, mOnConnectionFailedListener)
                 .addApi(Drive.API)
